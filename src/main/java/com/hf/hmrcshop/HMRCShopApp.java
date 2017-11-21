1562356d6ef443edc82235046798c1c5f25c9477
Stace C <stace@hiddenfield.com>
2017-11-21 00:32:52 +0000

    Initial Maven app structure in place, with empty tests.

---
+++ src/main/java/com/hf/hmrcshop/HMRCShopApp.java
@@ -0,0 +1,13 @@
+package com.hf.hmrcshop;
+
+/**
+ * HMRC Shop
+ *
+ */
+public class HMRCShopApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Customer!" );
+    }
+}
---
+++ src/test/java/com/hf/hmrcshop/HMRCShopTest.java
@@ -0,0 +1,38 @@
+package com.hf.hmrcshop;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class HMRCShopTest
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public HMRCShopTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( HMRCShopTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
